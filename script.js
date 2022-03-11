9c97632cd216133e2a00540e0368c71023c51d69
Jake Medley <jakemedley2014@gmail.com>
2022-03-10 20:38:23 -0600

    finished studio for fetch-json

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const container = document.getElementById("container");
+        let astronauts = "";
+        astronauts += `<p>Astronaut Count: ${json.length}</p>`;
+
+        for (astronaut of json.sort(function(a,b) {
+            return a.hoursInSpace - b.hoursInSpace;
+        })) {
+             astronauts += `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <li style="color:${astronaut.active ? "green" : "red"}">Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src=${astronaut.picture}>
+        </div>
+        `;
+    }
+        container.innerHTML = astronauts;
+       
+    });
+});
